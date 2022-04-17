e6e035a08f3feba8cf4cb653e3bb6d2a5fb004f4
Steffen Fieger <steffen@fieger-schortens.de>
2022-04-17 20:46:25 +0200

    feat: add emacs config

---
+++ .config/emacs/custom-vars.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(history-length 25))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-hspace ((t (:foreground "brightred" :underline t))))
+ '(whitespace-newline ((t (:foreground "brightgreen" :slant italic))))
+ '(whitespace-tab ((t (:foreground "brightred" :underline t :weight bold))))
+ '(whitespace-trailing ((t (:foreground "brightred" :underline t :weight normal)))))
